445752a1e88337f0576ba3993c799d85eda1db92
oyediranmohammed <95849758+oyediranmohammed@users.noreply.github.com>
2024-11-03 08:45:35 +0400

    python week one submission

---
+++ user_input.py
@@ -0,0 +1,6 @@
+print("Welcome!")
+name = input("Please, Enter your name: ")
+age = input("Please, Enter your age: ")
+location = input("Please, Enter your location: ")
+
+print(f'Hello {name}, you are welcome, this year, you are {age} years old and you live in Mohamme{location}.')
